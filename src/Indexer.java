ab6acfcbae1326998ee07532c3b7f0c5a85edcb0
Anuj Shah <Anuj Shah>
2017-02-28 22:05:14 -0800

    Attempted to parse HTML.

--- src/Indexer.java
+++ src/Indexer.java
@@ -1,3 +1,5 @@
+// Authors: Jeremy Yang, Anuj Shah, Jack Murray
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -6,6 +8,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.nio.charset.Charset;
 import java.nio.file.DirectoryStream;
@@ -19,6 +22,11 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Indexer 
 {
@@ -44,7 +52,7 @@ public class Indexer
     {
 		try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) 
     	{
-			for (Path entry : stream) 
+			for (Path entry: stream) 
 			{
 				if (Files.isDirectory(entry)) 
     	        {
@@ -57,14 +65,33 @@ public class Indexer
     	  
     static void i1() throws FileNotFoundException, IOException
     {
-		File [] s1 = Arrays.copyOfRange(listofFiles,0,37);
+		File[] s1 = Arrays.copyOfRange(listofFiles,0,37);
 		String line;
 		for (File f: s1)
-		{
+		{	
 			ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> map = new ConcurrentHashMap<>();
 			DirectoryStream<Path> stream = Files.newDirectoryStream(f.toPath());
 			for(Path file: stream)
 			{
+//				Document fileDoc = Jsoup.parse(file.toFile(), "UTF-8");
+//		    	Elements title = fileDoc.getElementsByTag("title");
+//		    	Elements h1 = fileDoc.getElementsByTag("h1");
+//		    	Elements h2 = fileDoc.getElementsByTag("h2");
+//		    	Elements h3 = fileDoc.getElementsByTag("h3");
+//		    	Elements bold = fileDoc.getElementsByTag("b");
+		    	
+		    	// System.out.println(fileDoc.text());
+//		    	for (Element e: title)
+//		    		System.out.println(e.text());
+//		    	for (Element e: h1)
+//		    		System.out.println(e.text());
+//		    	for (Element e: h2)
+//		    		System.out.println(e.text());
+//		    	for (Element e: h3)
+//		    		System.out.println(e.text());
+//		    	for (Element e: bold)
+//		    		System.out.println(e.text());
+		    	
 				try 
 				(
 				    InputStream fis = new FileInputStream(file.toFile());
@@ -74,9 +101,15 @@ public class Indexer
 				{
 				    while ((line = br.readLine()) != null) 
 				    {
+				    	// Document doc = Jsoup.parse(line);
+				    	// line = doc.text();
 				        String[] words = line.split(" ");
-				        for(String word:words)
+				        for(String word: words)
 				        {
+				        	if (word.equals("") || word.equals("@") || word.equals("&"))
+				        	{
+				        		continue;
+				        	}
 				        	addElement(map,word,f.getName());
 				        }
 				    }
@@ -108,8 +141,10 @@ public class Indexer
 				{
 				    while ((line = br.readLine()) != null) 
 				    {
+				    	// Document doc = Jsoup.parse(line);
+				    	// line = doc.text();
 				        String[] words = line.split(" ");
-				        for(String word : words)
+				        for(String word: words)
 				        {
 				        	addElement(map,word,f.getName());
 				        }
@@ -132,19 +167,22 @@ public class Indexer
 		         out.writeObject(map);
 		         out.close();
 		         fileOut.close();
-		         System.out.printf("Serialized data is saved:");
+		         System.out.printf("Serialized data is saved.");
 		  }
-		  catch(IOException i) 
+		  catch (IOException i) 
 		  {
 		      i.printStackTrace();
 		  }
 	}
 
-	public static void main(String[] args) throws InterruptedException, IOException 
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) throws InterruptedException, IOException, ClassNotFoundException 
 	{
-		splitFile(Paths.get("D:\\Desktop\\WEBPAGES_RAW"));
-		listofFiles=  allFiles.toArray(listofFiles);
-		ExecutorService service= Executors.newFixedThreadPool(2);
+		splitFile(Paths.get("C:\\Users\\anujs_000\\Desktop\\WEBPAGES_RAW"));
+		// splitFile(Paths.get("C:\\Users\\anujs_000\\Desktop\\Test"));
+		// splitFile(Paths.get("C:\\Desktop\\WEBPAGES_RAW"));
+		listofFiles = allFiles.toArray(listofFiles);
+		ExecutorService service = Executors.newFixedThreadPool(2);
 		service.submit(() -> {
 			try 
 			{
@@ -167,7 +205,23 @@ public class Indexer
 		});
 		service.awaitTermination(1, TimeUnit.SECONDS);
 		service.shutdown();
+//		try 
+//		{
+//			 ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> map;
+//	         FileInputStream fileIn = new FileInputStream("C:\\Users\\anujs_000\\Desktop\\Test\\4.ser");
+//	         ObjectInputStream in = new ObjectInputStream(fileIn);
+//	         map = (ConcurrentHashMap<String, ConcurrentLinkedQueue<String>>) in.readObject();
+//	         System.out.println(map);
+//	         in.close();
+//	         fileIn.close();
+//	      }
+//		catch(IOException i) 
+//		{
+//	         i.printStackTrace();
+//	         return;
+//		}
 //		for (String key:map.keySet())
 //			System.out.println(key + ":" +(map.get(key).toString()));
 	}
 }
+
