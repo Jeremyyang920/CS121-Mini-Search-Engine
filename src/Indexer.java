caa3ecf434e40b6f3a7c266a4967c185b781b99f
Anuj Shah <Anuj Shah>
2017-02-28 20:10:22 -0800

    Added a file of stopwords.

--- src/Indexer.java
+++ src/Indexer.java
@@ -20,14 +20,15 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class Indexer {
-    public static File [] listofFiles = new File [74];
-    public static ArrayList<File> allFiles= new ArrayList<File>();
+public class Indexer 
+{
+    public static File[] listofFiles = new File[74];
+    public static ArrayList<File> allFiles = new ArrayList<File>();
 
-    static void addElement(ConcurrentHashMap<String,ConcurrentLinkedQueue<String>>  map,String key,String value )
+    static void addElement(ConcurrentHashMap<String,ConcurrentLinkedQueue<String>>map, String key, String value)
     {
     	ConcurrentLinkedQueue<String> q = map.get(key);
-    	if(q == null) 
+    	if (q == null) 
     	{
     	  q = new ConcurrentLinkedQueue<String>();
     	  ConcurrentLinkedQueue<String> curQ = map.putIfAbsent(key, q);
@@ -41,31 +42,31 @@ public class Indexer {
     
     static ArrayList<File> splitFile(Path path) throws IOException
     {
-    	  try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) 
-    	  {
-    	        for (Path entry : stream) 
+		try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) 
+    	{
+			for (Path entry : stream) 
+			{
+				if (Files.isDirectory(entry)) 
     	        {
-    	            if (Files.isDirectory(entry)) 
-    	            {
-    	            	allFiles.add(entry.toFile());
-    	            }
-
+					allFiles.add(entry.toFile());
     	        }
-    	  }
+    	    }
+    	}
 		return allFiles;
     }
     	  
     static void i1() throws FileNotFoundException, IOException
     {
-		File [] s1=Arrays.copyOfRange(listofFiles,0,37);
+		File [] s1 = Arrays.copyOfRange(listofFiles,0,37);
 		String line;
 		for (File f: s1)
 		{
 			ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> map = new ConcurrentHashMap<>();
 			DirectoryStream<Path> stream = Files.newDirectoryStream(f.toPath());
-			for(Path file:stream)
+			for(Path file: stream)
 			{
-				try (
+				try 
+				(
 				    InputStream fis = new FileInputStream(file.toFile());
 				    InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
 				    BufferedReader br = new BufferedReader(isr);
@@ -78,30 +79,28 @@ public class Indexer {
 				        {
 				        	addElement(map,word,f.getName());
 				        }
-	
 				    }
 				}
 			}
 			outWrite(map,f.getAbsolutePath());
-			map=null;
+			map = null;
 			System.gc();
 			System.out.println(f.getAbsolutePath());
-			
 		}
-    	
     }
     
     static void i2() throws FileNotFoundException, IOException
     {
-		File [] s2=Arrays.copyOfRange(listofFiles, 37, 74);
+		File[] s2 = Arrays.copyOfRange(listofFiles, 37, 74);
 		String line;
 		for (File f: s2)
 		{
 			ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> map = new ConcurrentHashMap<>();
 			DirectoryStream<Path> stream = Files.newDirectoryStream(f.toPath());
-			for(Path file:stream)
+			for(Path file: stream)
 			{
-				try (
+				try 
+				(
 				    InputStream fis = new FileInputStream(file.toFile());
 				    InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
 				    BufferedReader br = new BufferedReader(isr);
@@ -110,38 +109,35 @@ public class Indexer {
 				    while ((line = br.readLine()) != null) 
 				    {
 				        String[] words = line.split(" ");
-				        for(String word:words)
+				        for(String word : words)
 				        {
 				        	addElement(map,word,f.getName());
 				        }
-	
 				    }
 				}
 			}
 			outWrite(map,f.getAbsolutePath());
-			map=null;
+			map = null;
 			System.gc();
 			System.out.println(f.getAbsolutePath());
-
 		}
     }
     
 	static synchronized void outWrite(ConcurrentHashMap<String,ConcurrentLinkedQueue<String>> map,String dir) throws IOException
 	{
-		  try {
-		         FileOutputStream fileOut =
-		         new FileOutputStream(dir+".ser");
+		  try 
+		  {
+		         FileOutputStream fileOut = new FileOutputStream(dir+".ser");
 		         ObjectOutputStream out = new ObjectOutputStream(fileOut);
 		         out.writeObject(map);
 		         out.close();
 		         fileOut.close();
 		         System.out.printf("Serialized data is saved:");
-		      }
+		  }
 		  catch(IOException i) 
 		  {
 		      i.printStackTrace();
 		  }
-
 	}
 
 	public static void main(String[] args) throws InterruptedException, IOException 
@@ -150,18 +146,22 @@ public class Indexer {
 		listofFiles=  allFiles.toArray(listofFiles);
 		ExecutorService service= Executors.newFixedThreadPool(2);
 		service.submit(() -> {
-			try {
+			try 
+			{
 				i1();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
+			} 
+			catch (IOException e) 
+			{
 				e.printStackTrace();
 			}
 		});
 		service.submit(() -> {
-			try {
+			try 
+			{
 				i2();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
+			} 
+			catch (IOException e) 
+			{
 				e.printStackTrace();
 			}
 		});
