8898ed81de113d5c0aa77fa0ecfcec40a52b4e44
Anuj Shah <Anuj Shah>
2017-03-15 23:48:14 -0700

    Added additional artifacts.

--- Tokens.sql
+++ Tokens.sql
@@ -1,3 +1,5 @@
+-- SET GLOBAL max_allowed_packet = 1073741824;
+
 -- DROP TABLE `Tokens`;
 
 -- CREATE TABLE `Tokens` (
@@ -5,13 +7,11 @@
 --  `document` TEXT(20000000)
 -- );
 
--- SET GLOBAL max_allowed_packet = 1073741824;
-
 -- SELECT COUNT(*) FROM TOKENS;
 
 -- OPTIMIZE TABLE `Tokens`;
 -- CREATE INDEX `token_index` ON `Tokens`(`token`) USING BTREE;
-SELECT * FROM `Tokens` WHERE token = 'anuj';
+-- SELECT * FROM `Tokens` WHERE token = 'anuj';
 
 -- DROP TABLE `H1`;
 
@@ -19,6 +19,8 @@ SELECT * FROM `Tokens` WHERE token = 'anuj';
 -- 	`token` VARCHAR(1000),
 --  `document` TEXT(20000000)
 -- );
+
+SELECT COUNT(*) FROM H1;
 
 -- OPTIMIZE TABLE `H1`;
 -- CREATE INDEX `h1_index` ON `H1`(`token`) USING BTREE;
@@ -30,6 +32,8 @@ SELECT * FROM `Tokens` WHERE token = 'anuj';
 --  `document` TEXT(20000000)
 -- );
 
+SELECT COUNT(*) FROM H2;
+
 -- OPTIMIZE TABLE `H2`;
 -- CREATE INDEX `h2_index` ON `H1`(`token`) USING BTREE;
 
@@ -40,15 +44,19 @@ SELECT * FROM `Tokens` WHERE token = 'anuj';
 --  `document` TEXT(20000000)
 -- );
 
+SELECT COUNT(*) FROM H3;
+
 -- OPTIMIZE TABLE `H3`;
 -- CREATE INDEX `h3_index` ON `H3`(`token`) USING BTREE;
 
 -- DROP TABLE `B`;
 
 -- CREATE TABLE `B` (
--- 	`token` VARCHAR(1000),
+--  `token` VARCHAR(1000),
 --  `document` TEXT(20000000)
 -- );
+
+SELECT COUNT(*) FROM B;
 
 -- OPTIMIZE TABLE `B`;
 -- CREATE INDEX `b_index` ON `B`(`token`) USING BTREE;
